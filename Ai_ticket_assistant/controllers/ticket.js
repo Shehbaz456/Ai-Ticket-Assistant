6988fa37b29fde47975950c95ff904ede151b616
shehbaz <mdshehbaz666666@gmail.com>
2025-07-03 17:26:32 +0530

    Implement ticket management with creation, retrieval, and Inngest integration

---
+++ Ai_ticket_assistant/controllers/ticket.js
@@ -0,0 +1,96 @@
+import { inngest } from "../inngest/client.js";
+import Ticket from "../models/ticket.js";
+
+export const createTicket = async (req, res) => {
+  try {
+    const { title, description } = req.body;
+    if (!title || !description) {
+      return res
+        .status(400)
+        .json({ message: "Title and description are required" });
+    }
+
+    const newTicket = await Ticket.create({
+      title,
+      description,
+      createdBy: req.user._id.toString(),
+    });
+
+    // Fire Inngest event for ticket creation
+    await inngest.send({
+      name: "ticket/created",
+      data: {
+        id: newTicket._id.toString(),
+        title: newTicket.title,
+        description: newTicket.description,
+        createdBy: req.user._id.toString(),
+      },
+    });
+
+    return res.status(201).json({
+      message: "Ticket created and processing started",
+      ticket: newTicket,
+    });
+  } catch (error) {
+    console.error("Error creating ticket:", error.message);
+    return res
+      .status(500)
+      .json({ error: "Internal server error", details: error.message });
+  }
+};
+
+export const getTickets = async (req, res) => {
+  try {
+    const user = req.user;
+    let tickets = [];
+    if (user.role !== "user") {
+      tickets = await Ticket.find({})
+        .populate("assignedTo", ["email", "_id"])
+        .sort({ createdAt: -1 });
+    } else {
+      tickets = await Ticket.find({ createdBy: user._id })
+        .select("title description status createdAt")
+        .sort({ createdAt: -1 })
+        .then((data) => {
+          tickets = data;
+        });
+    }
+    return res.status(200).json(tickets);
+  } catch (error) {
+    console.error("Error fetching tickets:", error.message);
+    return res
+      .status(500)
+      .json({ error: "Internal server error", details: error.message });
+  }
+};
+
+export const getTicket = async (req, res) => {
+  try {
+    const user = req.user;
+    const ticketId = req.params.id;
+    let ticket;
+
+    if (user.role !== "user") {
+      ticket = Ticket.findById(req.params.id).populate("assignedTo", [
+        "email",
+        "_id",
+      ]);
+    } else {
+      ticket = Ticket.findOne({
+        createdBy: user._id,
+        _id: req.params.id,
+      }).select("title description status createdAt");
+    }
+
+    if (!ticket) {
+      return res.status(404).json({ error: "Ticket not found" });
+    }
+
+    return res.status(200).json(ticket);
+  } catch (error) {
+    console.error("Error fetching ticket:", error.message);
+    return res
+      .status(500)
+      .json({ error: "Internal server error", details: error.message });
+  }
+};
--- Ai_ticket_assistant/index.js
+++ Ai_ticket_assistant/index.js
@@ -2,6 +2,12 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import cors from "cors";
+import userRoutes from "./routes/user.js";
+import ticketRoutes from "./routes/ticket.js";
+import { serve} from "inngest/express";
+import { inngest } from "./inngest/client.js";
+import { onUserSignup } from "./inngest/functions/on-signup.js";
+import { onTicketCreated } from "./inngest/functions/on-ticket-create.js";
 
 import connect_DB from "./db/index.js";
 
@@ -11,12 +17,14 @@ app.use(express.json());
 
 const PORT = process.env.PORT ?? 8000;
 
-
-// Import routes
-import userRoutes from "./routes/user.js";
-
 app.use("/api/auth", userRoutes);
+app.use("/api/tickets", ticketRoutes);
 
+// ingest middleware
+app.use("/api/ingest", serve({
+  client: inngest,
+  functions: [onUserSignup, onTicketCreated],
+}))
 
 connect_DB()
   .then(() => {
